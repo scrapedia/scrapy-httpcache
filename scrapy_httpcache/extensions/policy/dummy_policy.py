1e5c2e6a9a19344813bd253fe12b70c6dc8caec6
Grammy Jiang <grammy.jiang@gmail.com>
2019-12-19 22:42:15 +1100

    Add typing hint into DummyPolicy

--- scrapy_httpcache/extensions/policy/dummy_policy.py
+++ scrapy_httpcache/extensions/policy/dummy_policy.py
@@ -12,14 +12,14 @@ class DummyPolicy(object):
             int(x) for x in settings.getlist("HTTPCACHE_IGNORE_HTTP_CODES")
         ]
 
-    def should_cache_request(self, request):
+    def should_cache_request(self, request) -> bool:
         return urlparse_cached(request).scheme not in self.ignore_schemes
 
-    def should_cache_response(self, response, request):
+    def should_cache_response(self, response, request) -> bool:
         return response.status not in self.ignore_http_codes
 
-    def is_cached_response_fresh(self, cachedresponse, request):
+    def is_cached_response_fresh(self, cachedresponse, request) -> bool:
         return True
 
-    def is_cached_response_valid(self, cachedresponse, response, request):
+    def is_cached_response_valid(self, cachedresponse, response, request) -> bool:
         return True
