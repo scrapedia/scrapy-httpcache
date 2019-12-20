c83dd6786792d9053019894a2c31abc6f4881705
Grammy Jiang <gjiang@buildingsalive.com>
2019-12-20 21:25:39 +1100

    Add mongodb settings reading

--- scrapy_httpcache/extensions/cache_storage/mongo.py
+++ scrapy_httpcache/extensions/cache_storage/mongo.py
@@ -2,11 +2,13 @@
 The mongo cache storage
 """
 import logging
+import re
 from typing import Optional
 
 from motor.core import AgnosticClient as AsyncIOMotorClient
 from motor.core import AgnosticCollection as AsyncIOMotorCollection
 from motor.core import AgnosticDatabase as AsyncIOMotorDatabase
+from pymongo import ASCENDING
 from pymongo.collection import Collection
 from pymongo.database import Database
 from pymongo.mongo_client import MongoClient
@@ -16,6 +18,7 @@ from scrapy_httpcache import TRequest, TResponse, TSpider
 from scrapy_httpcache.extensions.cache_storage import CacheStorage
 
 logger = logging.getLogger(__name__)
+pattern = re.compile("^HTTPCACHE_MONGO_MONGOCLIENT_(?P<kwargs>(?!KWARGS).*)$")
 
 
 class MongoCacheStorage(CacheStorage):
@@ -26,6 +29,15 @@ class MongoCacheStorage(CacheStorage):
     def __init__(self, settings: Settings):
         super(MongoCacheStorage, self).__init__(settings)
 
+        self.settings = settings
+        self.mongo_settings = {
+            pattern.sub(lambda x: x.group(1).lower(), k): v
+            for k, v in filter(
+                lambda pair: pattern.match(pair[0]), settings.copy_to_dict().items()
+            )
+        }
+        self.mongo_settings.update(self.settings["HTTPCACHE_MONGO_MONGOCLIENT_KWARGS"])
+
         self.client: Optional[MongoClient] = None
         self.db: Optional[Database] = None
         self.collection: Optional[Collection] = None
@@ -35,8 +47,20 @@ class MongoCacheStorage(CacheStorage):
 
         :param spider:
         :type spider: TSpider
+        :return:
+        :rtype: None
         """
         # TODO: connect to the database
+        self.client: MongoClient = MongoClient(**self.mongo_settings)
+        self.db: Database = self.client.get_database(
+            **get_arguments(self.settings["HTTPCACHE_MONGO_DATABASE"])
+        )
+        self.collection: Collection = self.db.get_collection(
+            **get_arguments(self.settings["HTTPCACHE_MONGO_COLLECTION"])
+        )
+        self.collection.create_index([("key", ASCENDING)], unique=True)
+
+        logger.debug("Using MongoDB cache storage", extra={"spider": spider})
 
     def close_spider(self, spider: TSpider) -> None:
         """
